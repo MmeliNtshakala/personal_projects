1f0a3d27913a529bc7813cf268b6d8966c5e7302
MmeliNtshakala <mmeliroot@gmail.com>
2024-01-02 01:47:24 -0800

    password

---
+++ login/password.py
@@ -0,0 +1,9 @@
+print("LOGIN USER")
+userName = input("USERNAME >> ")
+password = input("PASSWORD >> ")
+if userName == "Mmeli" and password == "password":
+    print("Hey Mmeli")
+elif userName == userName and password == "password":
+    print("Hello welcome back", userName)
+else:
+    print("Wrong 'password'")
