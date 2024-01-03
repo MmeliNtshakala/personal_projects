d8440684f63a36c23c53d06f449ddc1cdadaae80
MmeliNtshakala <mmeliroot@gmail.com>
2024-01-03 00:54:51 -0800

    fix

--- take_order/customer.py
+++ take_order/customer.py
@@ -1,24 +1,24 @@
 print("Welcome to my resturant can i take your order")
 print("In our menu we have Chips, Hamburger and Pizza")
 order = input("What would you like to order? ")
-if order == "Hamburger" or "hamburger":
+if order == "Hamburger" or order == "hamburger":
     print("Great choice")
     cheese = input("Would you like some extra cheese? ")
-    if  cheese == "yes" or "Yes":
+    if  cheese == "yes" or cheese == "Yes":
         print("Hamburger with extra Cheese i got you")
     else:
         print("No extra cheese i got you")
-elif order == "Chips" or "chips":
+elif order == "Chips" or order == "chips":
     print("Awesome choice quick choice")
     cheese = input("Would you like some Chese sauce in there ? ")
-    if cheese == "Yes" or "yes":
+    if cheese == "Yes" or cheese == "yes":
         print("Chips with extra cheese coming up")
     else:
         print("Chips coming up")
-elif order == "Pizza" or "pizzza":
+elif order == "Pizza" or order == "pizzza":
     print("Grat Pizza coming up")
     paperoni = input("Do you want to add some peperoni? ")
-    if paperoni == "yes" or "Yes":
+    if paperoni == "yes" or paperoni == "Yes":
         print("Pizza with paperoni coming up")
     else:
         print("Pizza coming up you way")
